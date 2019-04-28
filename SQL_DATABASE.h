12bbf7f405e4f378235efab7c49c173c22d90f01
Z.W.Huang <ZhiwenHuang@pku.edu.cn>
2019-04-28 16:10:36 +0800

    initial database design

--- SQL_DATABASE.h
+++ SQL_DATABASE.h
@@ -1,6 +1,21 @@
+#include <string>
 class Database{
-	Database();
+	Database(const std::string Database_Name);
 	~Database();
+	void Read_Database();
 	void Add_Table();
 	void Select_Table();
+private:
+	int Num_of_Table;
+	std::string* Table_Pointer[];
+
 };
+
+/*
+	The stucture of database is initially imagined as belowing:
+	Each database has its own dictionary (as file folder) which holds
+		1.a .dtb file which describe the database;
+		2.several .tab file containing data of tables.
+	When database are created or chosen, it's initialized as a temp in 
+RAM and sync to files shortly after.
+ */
